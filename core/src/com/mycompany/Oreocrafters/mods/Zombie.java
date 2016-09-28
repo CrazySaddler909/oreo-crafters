7e1aaf0dc6a1ededd6ff4c15a4e6bddad69e979a
CrazySaddler909 <luke.mole2007@googlemail.com>
2016-09-28 20:05:27 +0100

    My first browser.Almost works

---
+++ core/src/com/mycompany/Oreocrafters/mods/Zombie.java
@@ -0,0 +1,21 @@
+package com.mycompany.Oreocrafters.mods;
+
+public class Zombie {
+	
+	public void create() {
+		
+	}
+	
+	public void render () {
+		
+	}
+	
+	public void height () {
+		
+	}
+	
+	public void width () {
+		
+	}
+	
+}
