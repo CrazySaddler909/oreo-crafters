5e3eb1f5fa95476577ac5924cacd360b2527b5d1
CrazySaddler909 <luke.mole2007@googlemail.com>
2017-02-12 20:04:39 +0000

    Stuff

--- html/src/com/mygdx/game/client/HtmlLauncher.java
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -3,6 +3,7 @@ package com.mygdx.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mycompany.Oreocrafters.MyGdxGame;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -13,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new Zombie_club_wedsite();
+                return new MyGdxGame();
         }
 }
