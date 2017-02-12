2ec18b6cc4d3a30488001d650e96a69cba250797
CrazySaddler909 <luke.mole2007@googlemail.com>
2017-02-12 20:26:59 +0000

    committing a mess

---
+++ core/src/com/mycompany/Oreocrafters/GameMenu.java
@@ -0,0 +1,26 @@
+package com.mycompany.Oreocrafters;
+
+public class GameMenu {
+
+	public void setSize(int i, int j) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test");
+	}
+
+	public void setLocation(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDefaultCloseOperation(int exitOnClose) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
